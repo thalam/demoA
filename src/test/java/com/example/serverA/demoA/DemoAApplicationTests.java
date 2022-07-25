97848bebb77e9ac3b7bb18f816fca1e942d77c8e
Jane Aarthy Joseph <jjose139@my.centennialcollege.ca>
2022-07-25 07:53:53 -0400

    First commit

---
+++ src/test/java/com/example/serverA/demoA/DemoAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.serverA.demoA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
