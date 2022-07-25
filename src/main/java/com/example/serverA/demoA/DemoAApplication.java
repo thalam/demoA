917221e91a59a351997ff6a341e81bad42d94693
Jane Aarthy Joseph <jjose139@my.centennialcollege.ca>
2022-07-25 10:00:55 -0400

    First commit

--- src/main/java/com/example/serverA/demoA/DemoAApplication.java
+++ src/main/java/com/example/serverA/demoA/DemoAApplication.java
@@ -1,13 +1,28 @@
 package com.example.serverA.demoA;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class DemoAApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoAApplication.class, args);
+	public static void main(String[] args)
+	{
+		// parameter ch
+		char ch = 'a';
+		// assigns character values
+		Character c = Character.valueOf(ch);
+
+
+		// assign hashcodes of c1, c2 to i1, i2
+		int i = c.hashCode();
+
+		// prints the character values
+		System.out.println("Hashcode of " + ch + " is " + i);
+		// b - 98
+		// B - 66
+		// a - 97
+		// A - 65
 	}
 
+	public int hashmap(){
+		return 1;
+	}
 }
--- src/test/java/com/example/serverA/demoA/DemoAApplicationTests.java
+++ src/test/java/com/example/serverA/demoA/DemoAApplicationTests.java
@@ -1,12 +1,8 @@
 package com.example.serverA.demoA;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class DemoAApplicationTests {
 
-	@Test
 	void contextLoads() {
 	}
 
